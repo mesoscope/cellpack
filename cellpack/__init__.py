130573a5990f2503bdf5808af7c4b8b128a6c2dc
meganrm <meganrm@gmail.com>
2021-07-30 16:12:02 -0700

    Bump version: 0.0.0 → 0.1.0

--- cellpack/__init__.py
+++ cellpack/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Megan Riel-Mehan"
 __email__ = "meganr@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 from .example import Example  # noqa: F401
 
--- setup.py
+++ setup.py
@@ -91,6 +91,6 @@ setup(
     url="https://github.com/mesoscope/cellpack",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.0",
+    version="0.1.0",
     zip_safe=False,
 )
