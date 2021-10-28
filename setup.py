dc982967e7d92dbf34fef2d3b344b6eb3ee15e44
meganrm <meganrm@gmail.com>
2021-10-28 16:52:57 -0700

    Bump version: 0.1.1 → 0.1.1

--- setup.py
+++ setup.py
@@ -94,6 +94,6 @@ setup(
     url="https://github.com/mesoscope/cellpack",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.1",
+    version="version="0.1.1"",
     zip_safe=False,
 )
