2573761f5ae2e9283312b076c5eaa37b4d015738
Saurabh Mogre <saurabh.mogre@alleninstitute.org>
2022-04-05 13:17:02 -0700

    formatting in progress

--- cellpack/autopack/ingredient/single_cube.py
+++ cellpack/autopack/ingredient/single_cube.py
@@ -55,7 +55,7 @@ class SingleCubeIngr(Ingredient):
         radii=None,
         rejectionThreshold=30,
         resolution_dictionary=None,
-        rotAxis=[0.0,0.0,0.0],
+        rotAxis=[0.0, 0.0, 0.0],
         rotRange=0,
         source=None,
         useOrientBias=False,
@@ -99,7 +99,7 @@ class SingleCubeIngr(Ingredient):
             useRotAxis=useRotAxis,
             weight=weight,
         )
-        
+
         print("MAKING CUBE", positions2)
         if name is None:
             name = "%5.2f_%f" % (radii[0][0], molarity)
@@ -108,18 +108,18 @@ class SingleCubeIngr(Ingredient):
         self.modelType = "Cube"
         self.collisionLevel = 0
         radii = numpy.array(radii)
-        
-        self.minRadius = min(radii[0]/2)  # should have three radii sizex,sizey,sizez
-        self.maxRadius = self.encapsulatingRadius = numpy.linalg.norm(radii[0]/2) # calculate encapsulating radius based on side length
-        self.bb = [-radii[0]/2, radii[0]/2]
-        
-        self.positions = positions # bottom left corner of cuboid
-        self.positions2 = positions2 # top right corner of cuboid
+
+        self.minRadius = min(radii[0] / 2)  # should have three radii sizex,sizey,sizez
+        self.maxRadius = self.encapsulatingRadius = numpy.linalg.norm(radii[0] / 2)  # calculate encapsulating radius based on side length
+        self.bb = [-radii[0] / 2, radii[0] / 2]
+
+        self.positions = positions  # bottom left corner of cuboid
+        self.positions2 = positions2  # top right corner of cuboid
         positions_ar = numpy.array(self.positions[0][0])
         positions2_ar = numpy.array(self.positions2[0][0])
-        
-        self.center = positions_ar+(positions2_ar-positions_ar)/2 #location of center based on corner points
-        
+
+        self.center = positions_ar+(positions2_ar-positions_ar) / 2  # location of center based on corner points
+
         self.radii = radii
 
     def collision_jitter(
@@ -162,35 +162,35 @@ class SingleCubeIngr(Ingredient):
                 self.vi.updateBox(box, cornerPoints=bb)
             self.vi.update()
 
-        points_to_check = histoVol.grid.getPointsInCube(bb, center_trans, search_radius) # indices of all grid points within padded distance from cube center
-        
+        points_to_check = histoVol.grid.getPointsInCube(bb, center_trans, search_radius)  # indices of all grid points within padded distance from cube center
+
         grid_point_vectors = numpy.take(gridPointsCoords, points_to_check, 0) - center_trans  # vectors joining center of cube with grid points
-        grid_point_distances = numpy.linalg.norm(grid_point_vectors,axis=1) # distances of grid points from packing location
-        
+        grid_point_distances = numpy.linalg.norm(grid_point_vectors,axis=1)  # distances of grid points from packing location
+
         for pti in range(len(points_to_check)):
             # pti = point index
-            
+
             grid_point_index = points_to_check[pti]
-            
+
             if grid_point_index in insidePoints:
                 continue
-          
-            collision = grid_point_distances[pti]+current_grid_distances[grid_point_index]<=self.encapsulatingRadius
-           
+
+            collision = grid_point_distances[pti] + current_grid_distances[grid_point_index] <= self.encapsulatingRadius
+
             if collision:
                 self.log.info("grid point already occupied %f", grid_point_index)
                 return True, {}, {}
-                        
+
             # currently calculates distance from surface of encapsulating sphere
             # should be updated to distance from cube surface
-            signed_distance_to_cube_surface = grid_point_distances[pti]-self.encapsulatingRadius
+            signed_distance_to_cube_surface = grid_point_distances[pti] - self.encapsulatingRadius
             if(
                 signed_distance_to_cube_surface<=0
             ):
                 # check if grid point lies inside the cube
                 insidePoints[grid_point_index] = signed_distance_to_cube_surface
             elif(
-                signed_distance_to_cube_surface<=current_grid_distances[grid_point_index]
+                signed_distance_to_cube_surface <= current_grid_distances[grid_point_index]
             ):
                 # update grid distances if no collision was detected
                 if grid_point_index in newDistPoints:
