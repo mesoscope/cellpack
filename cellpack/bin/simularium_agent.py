db4e7b5dd91dec8d3efd7a28f3d287410bc072c1
Saurabh Mogre <saurabh.mogre@alleninstitute.org>
2022-04-27 15:54:38 -0700

    functions to calculate and plot MSD scaling

--- cellpack/bin/simularium_agent.py
+++ cellpack/bin/simularium_agent.py
@@ -23,16 +23,19 @@ class simularium_agent:
         self.type_ID = type_ID
 
     def append_position(self, input_position):
+    # adds a position to an agent
         self.position.append(input_position)
 
     def append_rotation(self, input_rotation):
+    # adds a rotation to an agent        
         self.rotation.append(input_rotation)
 
     def append_time_point(self, current_time):
+    # adds a time point to an agent
         self.time_points.append(current_time)
 
-    def get_single_MSD(self):
-        # calculates brute force MSD
+    def get_time_averaged_MSD(self):
+    # calculates the time averaged MSD for a single agent
         positions = numpy.array(self.position, dtype=object, copy=False)
         time_shift = numpy.arange(len(positions))
         MSD = numpy.zeros(len(time_shift))
@@ -44,11 +47,46 @@ class simularium_agent:
         self.MSD = MSD
         
         return MSD
+
+    @staticmethod
+    def get_ensemble_MSD(agent_dict):
+    # gets the ensemble averaged MSD
+    # averages are performed over agents of the same type
+    # assumes that all time points are evenly spaced
+        ensemble_MSD = {}
+        ensemble_counts = {}
+
+        for agent in agent_dict.values():
+            if agent.type_ID in ensemble_MSD:
+                n_points = len(agent.MSD)
+
+                tmp_MSD = ensemble_MSD[agent.type_ID]
+                tmp_MSD[0:n_points] = tmp_MSD[0:n_points] + agent.MSD
+                ensemble_MSD[agent.type_ID] = tmp_MSD
+                
+                tmp_count = ensemble_counts[agent.type_ID]
+                tmp_count[0:n_points] += 1
+                ensemble_counts[agent.type_ID] = tmp_count
+
+            else:
+                n_points = len(agent.MSD)
+                ensemble_MSD[agent.type_ID] = agent.MSD
+                tmp = numpy.ones(n_points)
+                ensemble_counts[agent.type_ID] = tmp
+
+        time_points = {}
+        for agent_type in ensemble_MSD:
+            ensemble_MSD[agent_type] = ensemble_MSD[agent_type]/ensemble_counts[agent_type]
+            time_points[agent_type] = numpy.arange(len(ensemble_MSD[agent_type]))
+        
+        return time_points, ensemble_MSD
+
     
     @classmethod
     def get_MSD_for_all_agents(self,agent_dict):
+    # calculates the MSD for all agents
         for agent in agent_dict.values():
-            self.MSD = self.get_single_MSD(agent)
+            self.MSD = self.get_time_averaged_MSD(agent)
 
     @classmethod
     def parse_simularium_file(self, data_file):
@@ -75,6 +113,35 @@ class simularium_agent:
                                             line_counter,
                                             )
         return agent_dict, time_step_size, time_units, space_units
+
+    @staticmethod
+    def enumerate_agents(agent_dict):
+        agent_counts = {}
+        for agent in agent_dict.values():
+            if agent.type_ID in agent_counts:
+                agent_counts[agent.type_ID]['count'] += 1
+            else:
+                agent_counts[agent.type_ID] = {}
+                agent_counts[agent.type_ID]['count'] = 1
+                agent_counts[agent.type_ID]['radius'] = agent.radius
+        return agent_counts
+
+    @staticmethod
+    def get_MSD_scaling(time_points, MSD):
+    # calculates the scaling alpha of the MSD
+    # MSD = 2D * time_points^alpha
+        if not MSD[0]:
+            MSD = MSD[1:]
+        if not time_points[0]:
+            time_points = time_points[1:]
+        log_MSD = numpy.log(MSD)
+        log_time = numpy.log(time_points)
+
+        mult_matrix = numpy.vstack([log_time, numpy.ones(len(log_time))]).T
+        scaling, intercept = numpy.linalg.lstsq(mult_matrix, log_MSD, rcond=None)[0]
+        diffusion_coefficient = numpy.exp(intercept) / 2.0
+
+        return diffusion_coefficient, scaling
 
     @staticmethod
     def update_agent_info(agent_dict, current_time, all_data, line_counter):
