7f1f7280eeb1ff581785c598bf00d06eeca42f82
Ruge Li <rugeli0605@gmail.com>
2022-09-12 14:22:45 -0700

    rename dict file

---
+++ cellpack/autopack/loaders/v1_v2_attribute_changes.py
@@ -0,0 +1,21 @@
+v1_to_v2_name_map = {
+    "Type": "type",
+    "distExpression": "distance_expression",
+    "distFunction": "distance_function",
+    "isAttractor": "is_attractor",
+    "jitterMax": "max_jitter",
+    "nbJitter": "num_jitter",
+    "nbMol": "count",
+    "overwrite_distFunc": "	overwrite_dist_func",
+    "packingMode": "packing_mode",
+    "packingPriority": "packing_priority",
+    "perturbAxisAmplitude": "perturb_axis_amplitude",
+    "placeType": "place_type",
+    "principalVector": "principal_vector",
+    "rejectionThreshold": "rejection_threshold",
+    "rotAxis": "rotation_axis",
+    "rotRange": "rotation_range",
+    "useRotAxis": "use_rotation_axis",
+}
+
+unused_attributes_list = ["encapsulatingRadius","meshObject","meshType","name","properties"]
