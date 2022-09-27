fc39b2c3980ebcf43b3308c2ab0e08598058581c
Saurabh Mogre <saurabh.mogre@alleninstitute.org>
2022-09-27 16:57:49 -0700

    updating log file path

--- cellpack/bin/pack.py
+++ cellpack/bin/pack.py
@@ -12,8 +12,7 @@ from cellpack.autopack.loaders.config_loader import ConfigLoader
 from cellpack.autopack.loaders.recipe_loader import RecipeLoader
 
 ###############################################################################
-
-log_file_path = path.join(path.dirname(path.abspath(__file__)), "../../logging.conf")
+log_file_path = path.abspath(path.join(__file__, "../../../logging.conf"))
 logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
 log = logging.getLogger()
 ###############################################################################
