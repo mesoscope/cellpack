dc62c2d5808bc2a17096ef12c17377482feb2cb7
Ruge Li <rugeli0605@gmail.com>
2022-12-12 21:59:01 -0800

    more work with crud, needs testing

--- cellpack/autopack/firebase.py
+++ cellpack/autopack/firebase.py
@@ -1,6 +1,5 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 import copy
 
 
@@ -36,20 +35,22 @@ class FirebaseHandler(object):
 
     # add documents with auto IDs
     def to_firestore(self, collection, data, id=None):
+        # check if we need to convert part of the data(2d arrays and objs to dict) 
+        modified_data = self.reconstruct_dict(data)
         # check if the incoming new data exists in db
         if id is None: 
             data_ref = self.db.collection(collection)
-            name = data["name"]
+            name = modified_data["name"]
             query_ref = data_ref.where("name", "==", name)
             docs = query_ref.get()
-            if docs and data.isEqual(docs):
+            if docs and modified_data.isEqual(docs):
                 print("this data already exists in firestore")
             else: 
-                doc_ref = self.db.collection(collection).add(data)
+                doc_ref = self.db.collection(collection).add(modified_data)
                 print(f"successfully uploaded to path: {doc_ref.path}")
         #TODO: check when the id will not be None
         else:
-            doc_ref = self.db.collection(collection).add(data)
+            doc_ref = self.db.collection(collection).add(modified_data)
         return doc_ref
 
     def upload_recipe(self, recipe_data):
@@ -57,10 +58,6 @@ class FirebaseHandler(object):
         recipe_version = recipe_data["version"]
         key = f"{recipe_name}_v{recipe_version}"
         # TODO: check we want to use version or format_version 
-        # if doc.exists()
-            # if it already does, throw error, tell user to version the recipe
-            # if they still want to upload it 
-        # LONGER TERM: could check to see if all the data is the same and let the user know 
         recipe_ref = self.to_firestore("recipes", recipe_data, key)
         print(f"successfully uploaded recipe to path: {recipe_ref.path}")
 
@@ -90,38 +87,55 @@ class FirebaseHandler(object):
                             obj_name = region_item["object"]
                             region_item["object"] = objects_to_path_map.get(obj_name)
                         else: 
-                            #what cases are for else? 
+                            #update str with comp_path, but we need a second loop since the contained comp obj has not been saved to db yet
                             pass
             comp_obj["name"] = comp_name
             comp_ref = self.to_firestore("composition", comp_obj)
             comp_to_path_map[comp_name] = comp_ref.path
-            recipe_to_save["composition"][comp_name] = { "inherit" : comp_to_path_map[comp_name] }
-            #TODO: update comp path -- i.e. replace "nucleus" with nucleus comp path in membrane comp              
-
+            recipe_to_save["composition"][comp_name] = { "inherit" : comp_to_path_map[comp_name] }            
             self.upload_recipe(recipe_to_save)
+        self._update_comp_ref_in_comp_obj(comp_to_path_map)
+    
+    #TODO: logic is not well, needs refine
+    def _update_comp_ref_in_comp_obj(self,comp_to_path_map):
+        for comp_name, comp_path in comp_to_path_map.items():
+            comp_doc = self.db.document(comp_path).get()
+            if "regions" in comp_doc.to_dict():
+                for region_name, region_array in comp_doc.to_dict()["regions"].items():
+                    for index in range(len(region_array)):
+                        region_item = region_array[index]
+                        if isinstance(region_item, str):
+                            value_path = "regions.{region_array}.{index}"
+                            new_value = comp_to_path_map.get(region_item)
+                            comp_doc.update({
+                                value_path: new_value
+                            })
+
+        #this query will get all docs that have "regions"          
+        # docs = self.db.collection("composition").where("regions", ">", "").get()
+            
+        
+    def get_collection_id_from_path(self, path_to_recipe):
+        #path example = composition/uid_1
+        components = path_to_recipe.split("/")
+        collection = components[0]
+        id = components[1]
+        return collection, id
+
+        #however we can directly get doc by path  
+        #doc = self.db.document(path_to_recipe).get()
+        
 
     # get a document with a known ID
-    # def get_doc_from_firestore(collection, id):
-    #     doc = db.collection(collection).document(id).get()
-    #     if doc.exists:
-    #         return doc.to_dict()
-    #     else:
-    #         return "requested doc doesn't exist in firestore"
-        get_collection_id_from_path(self )
+    def read_from_firestore(self, collection, id):
+        doc = self.db.collection(collection).document(id).get()
+        if doc.exists:
+            return doc.to_dict()
+        else:
+            return "requested doc doesn't exist in firestore"
 
-    # # get all documents in a collection
-    # def get_all_docs_from_firestore(collection):
-    #     docs = db.collection(collection).get()
-    #     docs_list = []
-    #     for doc in docs:
-    #         docs_list.append(doc.to_dict())
-    #     print(docs_list)
-    #     return docs_list
-        def read(self, collection, id):
-            return self.db.collection(collection).get(id)
-
-        def read_recipe(self, path_to_recipe):
-            collection, id = self.get_collection_id_from_path(path_to_recipe)
-            data_from_firebase = self.read(collection, id)
-            # TODO: convert to recipe that looks like it was read from a file
-            # return converted data
+    def read_recipe(self, path_to_recipe):
+        collection, id = self.get_collection_id_from_path(path_to_recipe)
+        data_from_firebase = self.read_from_firestore(collection, id)
+        # TODO: convert to recipe that looks like it was read from a file
+        # return converted data
