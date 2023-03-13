256e97baa4b85c92bdeae7b1ff345319d0ba204e
meganrm <meganrm@gmail.com>
2023-03-13 14:20:49 -0700

    remove commented code

--- cellpack/autopack/interface_objects/partners.py
+++ cellpack/autopack/interface_objects/partners.py
@@ -21,31 +21,6 @@ class Partner:
             return None
         return self.points[index]
 
-    # def setup(
-    #     self,
-    # ):
-    # QUESTION: why is this commented out?
-    # # setup the marge according the pt properties
-    # pt1 = numpy.array(self.getProperties("pt1"))
-    # pt2 = numpy.array(self.getProperties("pt2"))
-    # pt3 = numpy.array(self.getProperties("pt3"))
-    # pt4 = numpy.array(self.getProperties("pt4"))
-
-    # # length = autopack.helper.measure_distance(pt2,pt3)#length
-    # margein = math.degrees(
-    #     autopack.helper.angle_between_vectors(pt2 - pt1, pt3 - pt2)
-    # )  # 4
-    # margeout = math.degrees(
-    #     autopack.helper.angle_between_vectors(pt3 - pt2, pt4 - pt3)
-    # )  # 113
-    # dihedral = math.degrees(
-    #     autopack.helper.angle_between_vectors(pt2 - pt1, pt4 - pt2)
-    # )  # 79
-    # dihedral = autopack.helper.dihedral(pt1, pt2, pt3, pt4)
-    # self.properties["marge_in"] = [margein - 1, margein + 1]
-    # self.properties["marge_out"] = [margeout - 1, margeout + 1]
-    # self.properties["diehdral"] = [dihedral - 1, dihedral + 1]
-
     def distanceFunction(self, d, expression=None, function=None):
         # default function that can be overwrite or
         # can provide an experssion which 1/d or 1/d^2 or d^2etc.w*expression
