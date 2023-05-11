1f881c83a54be04497529458298dd88b25fdcba0
Ruge Li <rugeli0605@gmail.com>
2023-05-11 12:55:30 -0700

    fix test

--- cellpack/autopack/DBRecipeHandler.py
+++ cellpack/autopack/DBRecipeHandler.py
@@ -297,11 +297,17 @@ class GradientDoc(DataDoc):
         super().__init__()
         self.settings = settings
 
+    def as_dict(self):
+        data = dict()
+        for key in self.settings:
+            data[key] = self.settings[key]
+        return data
+
     def should_write(self, db, grad_name):
         docs = db.get_doc_by_name("gradients", grad_name)
         if docs and len(docs) >= 1:
             for doc in docs:
-                local_data = DBRecipeHandler.prep_data_for_db(db.doc_to_dict(doc))
+                local_data = DBRecipeHandler.prep_data_for_db(self.as_dict())
                 db_data = db.doc_to_dict(doc)
                 difference = DeepDiff(db_data, local_data, ignore_order=True)
                 if not difference:
