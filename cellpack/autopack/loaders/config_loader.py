0251da9234dc15c109ba566ad2bcdde9d7b8d2f4
meganrm <meganrm@gmail.com>
2023-10-27 14:54:21 -0700

    default upload_results to true

--- cellpack/autopack/loaders/config_loader.py
+++ cellpack/autopack/loaders/config_loader.py
@@ -43,7 +43,7 @@ class ConfigLoader(object):
         "show_sphere_trees": False,
         "show_progress_bar": False,
         "spacing": None,
-        "upload_results": False,
+        "upload_results": True,
         "use_periodicity": False,
         "version": 1.0,
     }
