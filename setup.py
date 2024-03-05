87884c14596c908a288c5069a582d907bed0b37f
Karthik Vegesna <krvegesna@gmail.com>
2024-03-05 14:46:15 -0800

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -118,6 +118,6 @@ setup(
     url="https://github.com/mesoscope/cellpack",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.0.3",
+    version="1.0.4",
     zip_safe=False,
 )
