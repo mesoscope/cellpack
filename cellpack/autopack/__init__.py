843dfec3b3a5b05843ebf5049aa2c522b27dcc5a
Ruge Li <91452427+rugeli@users.noreply.github.com>
2024-05-16 11:32:21 -0700

    call path validation when initiating the packing for remote recipes (#248)

--- cellpack/autopack/__init__.py
+++ cellpack/autopack/__init__.py
@@ -393,6 +393,7 @@ def load_file(filename, destination="", cache="geometries", force=None):
                     f"The selected database is not initialized. Please set up Firebase credentials to pack remote recipes. Refer to the instructions at {readme_url}"
                 )
             db_handler = DBRecipeLoader(initialize_db)
+            db_handler.validate_input_recipe_path(filename)
             recipe_id = file_path.split("/")[-1]
             db_doc, _ = db_handler.collect_docs_by_id(
                 collection="recipes", id=recipe_id
