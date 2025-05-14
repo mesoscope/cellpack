4de0c12588e482e0a4751331c543751271f4e101
ascibisz <alli.scibisz@alleninstitute.org>
2025-05-14 16:34:02 -0700

    Add Dockerfile.ecs for our ECS image and rename old Dockerfile to Dockerfile.batch

---
+++ docker/server.py
@@ -0,0 +1,32 @@
+from aiohttp import web
+import os
+import uuid
+from cellpack.bin.pack import pack
+
+SERVER_PORT = 8443
+
+async def hello_world(request: web.Request) -> web.Response:
+    return web.Response(text="Hello world")
+
+async def pack_handler(request: web.Request) -> web.Response:
+    recipe = request.rel_url.query.get("recipe")
+    if recipe is None:
+        # pack request must include "recipe" query parameter
+        raise web.HTTPBadRequest()
+    config = request.rel_url.query.get("config")
+    job_id = str(uuid.uuid4())
+    os.environ["AWS_BATCH_JOB_ID"] = job_id
+    pack(recipe=recipe, config_path=config, docker=True)
+    return web.json_response({"job_id": job_id})
+
+async def init_app() -> web.Application:
+    app = web.Application()
+    app.add_routes(
+        [
+            web.get("/hello", hello_world),
+            web.get("/pack", pack_handler),
+        ]
+    )
+    return app
+
+web.run_app(init_app(), host="0.0.0.0", port=SERVER_PORT)
