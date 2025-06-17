afab106d30ebbdaae3b389e8ba40aa9ab1674558
ascibisz <alli.scibisz@alleninstitute.org>
2025-06-17 15:44:48 -0700

    revert accidential change to is_remote_path

--- cellpack/autopack/__init__.py
+++ cellpack/autopack/__init__.py
@@ -397,26 +397,13 @@ def load_file(
 ):
     if is_remote_path(filename):
         database_name, file_path = convert_db_shortname_to_url(filename)
-        # command example: `pack -r firebase:recipes/[FIREBASE-RECIPE-ID] -c [CONFIG-FILE-PATH]`
-        if database_name == "firebase":
-            db = DATABASE_IDS.handlers().get(database_name)
-            initialize_db = db(default_db="staging") if use_docker else db()
-            if not initialize_db._initialized:
-                readme_url = "https://github.com/mesoscope/cellpack?tab=readme-ov-file#introduction-to-remote-databases"
-                sys.exit(
-                    f"The selected database is not initialized. Please set up Firebase credentials to pack remote recipes. Refer to the instructions at {readme_url}"
-                )
-            db_handler = DBRecipeLoader(initialize_db)
-            db_handler.validate_input_recipe_path(filename)
-            recipe_id = file_path.split("/")[-1]
-            db_doc, _ = db_handler.collect_docs_by_id(
-                collection="recipes", id=recipe_id
-            )
-            downloaded_recipe_data = db_handler.prep_db_doc_for_download(db_doc)
-            return downloaded_recipe_data, database_name
-        else:
-            local_file_path = get_local_file_location(
-                file_path, destination=destination, cache=cache, force=force
+        db = DATABASE_IDS.handlers().get(database_name)
+        initialize_db = db(default_db="staging") if use_docker else db()
+
+        if not initialize_db._initialized:
+            readme_url = "https://github.com/mesoscope/cellpack?tab=readme-ov-file#introduction-to-remote-databases"
+            sys.exit(
+                f"The selected database: {database_name} is not initialized. Please set up credentials to pack remote recipes. Refer to the instructions at {readme_url}"
             )
         db_handler = DBRecipeLoader(initialize_db)
         db_handler.validate_input_recipe_path(filename)
