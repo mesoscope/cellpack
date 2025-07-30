98d3261cf61ef8b231569b8aeae2d2a0f0eabce0
Megan Riel-Mehan <meganrm@gmail.com>
2025-07-30 13:32:28 -0700

    Update cellpack/autopack/loaders/migrate_v2_to_v2_2.py
    Co-authored-by: Ruge Li <91452427+rugeli@users.noreply.github.com>

--- cellpack/autopack/loaders/migrate_v2_to_v2_2.py
+++ cellpack/autopack/loaders/migrate_v2_to_v2_2.py
@@ -6,8 +6,8 @@ from ..interface_objects.gradient_data import GradientData, ModeOptions
 def convert(recipe_data_2_1):
     new_recipe = copy.deepcopy(recipe_data_2_1)
     new_recipe["format_version"] = "2.2"
-    for object_name in recipe_data_2_1["objects"]:
-        object_data = recipe_data_2_1["objects"][object_name]
+    for object_name in new_recipe["objects"]:
+        object_data = new_recipe["objects"][object_name]
         if "packing_mode" not in object_data:
             object_data["packing_mode"] = "random"
         else:
