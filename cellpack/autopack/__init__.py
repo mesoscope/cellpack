565df855d7ed3a5ce67de31fd2a5c1c1cef4b4c1
Alli <111383930+ascibisz@users.noreply.github.com>
2025-08-13 10:05:43 -0700

    Feature/edited recipes in firebase (#350)
    * tFeature/aws cli in docker (#280)
    
    * Add basic Dockerfile and docker-entrypoint file
    
    * draft aws cli inside of docker container
    
    * access aws s3 with saved cred
    
    * update readme
    
    * update docker commands
    
    * update readme
    
    * update readme
    
    ---------
    
    Co-authored-by: ascibisz <alli.scibisz@alleninstitute.org>
    
    * Feature/pass recipe to docker (#285)
    
    * draft aws cli inside of docker container
    
    * access aws s3 with saved cred
    
    * update readme
    
    * update docker commands
    
    * update readme
    
    * update readme
    
    * add parameter for when we're using docker to default to using the staging firebase db
    
    * read recipe and config in from docker run params
    
    * use dockerignore file to prevent bloat in docker image
    
    * add `batch_job_id` to result metadata (#295)
    
    * retrieve job id
    
    * formatting
    
    * add a bucket for batch jobs
    
    * formatting
    
    * remove print statement and add comments
    
    ---------
    
    Co-authored-by: Ruge Li <rugeli0605@gmail.com>
    Co-authored-by: Ruge Li <91452427+rugeli@users.noreply.github.com>
    
    * pull changes for firebase configs handling
    
    * Feature/docker file for ecs (#336)
    
    * Add Dockerfile.ecs for our ECS image and rename old Dockerfile to Dockerfile.batch
    
    * add intentional error handling for if packing fails
    
    * change messaging from /hello endpoint
    
    * add healthcheck endpoint and use port 80 instead of 8443 for load balancer requirements
    
    * add way for running ECS docker image locally without trying to publish results to cellpack-demo/
    
    * revert accidential change to is_remote_path
    
    * remove is_remote flag
    
    * temporarily use cellpack-results bucket
    
    * revert demo bucket name change
    
    * make changes to cellpack code to accept a remote recipe that closely resembles a local recipe
    
    * lint fix
    
    * cleanup code
    
    * use local variable to improve readability
    
    * lint fix
    
    ---------
    
    Co-authored-by: Ruge Li <91452427+rugeli@users.noreply.github.com>
    Co-authored-by: Ruge Li <rugeli0605@gmail.com>

--- cellpack/autopack/__init__.py
+++ cellpack/autopack/__init__.py
@@ -408,14 +408,16 @@ def load_file(
         db_handler = DBRecipeLoader(initialize_db)
         db_handler.validate_input_recipe_path(filename)
         recipe_id = file_path.split("/")[-1]
-        db_doc, _ = db_handler.collect_docs_by_id(collection="recipes", id=recipe_id)
+        collection = file_path.split("/")[0]
+        db_doc, _ = db_handler.collect_docs_by_id(collection=collection, id=recipe_id)
         downloaded_recipe_data = db_handler.prep_db_doc_for_download(db_doc)
-        return downloaded_recipe_data, database_name
+        is_unnested_collection = collection == "recipes_edited"
+        return downloaded_recipe_data, database_name, is_unnested_collection
     else:
         local_file_path = get_local_file_location(
             filename, destination=destination, cache=cache, force=force
         )
-        return json.load(open(local_file_path, "r")), None
+        return json.load(open(local_file_path, "r")), None, False
 
 
 def fixPath(adict):  # , k, v):
