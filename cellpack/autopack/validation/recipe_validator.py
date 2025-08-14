47f6e2995d48b7208ec109e50897bceea172f68e
Ruge Li <rugeli0605@gmail.com>
2025-08-14 12:37:36 -0700

    simplify recipe validator and catch the error

--- cellpack/autopack/validation/recipe_validator.py
+++ cellpack/autopack/validation/recipe_validator.py
@@ -1,23 +1,19 @@
-from pydantic import ValidationError
 from .recipe_models import Recipe, RecipeObject
-
 
 
 class RecipeValidator:
     @staticmethod
     def validate_recipe(recipe_data):
-        try:
-            recipe_model = Recipe(**recipe_data)
-            validated_data = recipe_model.model_dump() #equivalent to .dict()   
-            
-            if "objects" in recipe_data and recipe_data["objects"]:
-                validated_objects = {}
-                for obj_name, obj_data in recipe_data["objects"].items():
-                    validated_obj = RecipeObject(**obj_data)
-                    validated_objects[obj_name] = validated_obj.model_dump()
-                validated_data["objects"] = validated_objects
-            
-            return validated_data
-            
-        except ValidationError as e:
-            print("Validation error:", e)
+        # Validate the main recipe structure
+        recipe_model = Recipe(**recipe_data)
+        validated_data = recipe_model.model_dump()  # equivalent to .dict()
+
+        # Validate individual objects
+        if "objects" in recipe_data and recipe_data["objects"]:
+            validated_objects = {}
+            for obj_name, obj_data in recipe_data["objects"].items():
+                validated_obj = RecipeObject(**obj_data)
+                validated_objects[obj_name] = validated_obj.model_dump()
+            validated_data["objects"] = validated_objects
+
+        return validated_data
