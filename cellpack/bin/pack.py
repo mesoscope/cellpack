3b31e543ce628b372c87aa06e7926c0e5d1ef763
Ruge Li <rugeli0605@gmail.com>
2025-08-14 13:23:09 -0700

    draft add validate to pack

--- cellpack/bin/pack.py
+++ cellpack/bin/pack.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import logging
 import logging.config
 import time
+import sys
 
 from cellpack import autopack
 from cellpack.autopack import upy
@@ -12,6 +13,10 @@ from cellpack.autopack.Environment import Environment
 from cellpack.autopack.loaders.config_loader import ConfigLoader
 from cellpack.autopack.loaders.recipe_loader import RecipeLoader
 from cellpack.autopack.loaders.analysis_config_loader import AnalysisConfigLoader
+from cellpack.autopack.validation.recipe_validator import RecipeValidator
+
+from pydantic import ValidationError
+import json
 
 ###############################################################################
 log_file_path = Path(__file__).parent.parent / "logging.conf"
@@ -20,17 +25,32 @@ log = logging.getLogger()
 ###############################################################################
 
 
-def pack(recipe, config_path=None, analysis_config_path=None, docker=False):
+def pack(
+    recipe, config_path=None, analysis_config_path=None, docker=False, validate=True
+):
     """
     Initializes an autopack packing from the command line
     :param recipe: string argument, path to recipe
     :param config_path: string argument, path to packing config file
     :param analysis_config_path: string argument, path to analysis config file
     :param docker: boolean argument, are we using docker
+    :param validate: boolean argument, validate recipe before packing
 
     :return: void
     """
     packing_config_data = ConfigLoader(config_path, docker).config
+
+    # validate recipe before packing
+    if validate:
+        try:
+            with open(recipe, "r") as f:
+                raw_recipe_data = json.load(f)
+            RecipeValidator.validate_recipe(raw_recipe_data)
+            log.info("Recipe validation passed!")
+        except ValidationError as e:
+            log.error(f"Recipe validation failed: {e}")
+            sys.exit(1)
+
     recipe_data = RecipeLoader(
         recipe, packing_config_data["save_converted_recipe"], docker
     ).recipe_data
