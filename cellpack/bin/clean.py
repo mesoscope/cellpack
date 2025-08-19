1c0cbf5a145885a910ff846389d2453c1983642b
mogres <saurabh.mogre@alleninstitute.org>
2025-08-19 15:31:15 -0700

    Refactor cache cleaning function to improve error handling and logging

--- cellpack/bin/clean.py
+++ cellpack/bin/clean.py
@@ -1,26 +1,43 @@
 # cleans the local cache directory
+import logging
 import shutil
-from cellpack.autopack import CACHE_DIR
+from pathlib import Path
+
 import fire
-import os
+
+from cellpack.autopack import appdata
+
+log = logging.getLogger(__name__)
 
 
-def clean():
+def clean(cache_dir_path: Path | str = appdata):
     """
-    Cleans the local cache directory
-    :return: void
+    Cleans the local cache directories.
+
+    Parameters
+    ----------
+    cache_dir_path
+        The path to the cache directory. Defaults to the appdata directory.
     """
-    for _, folder in CACHE_DIR.items():
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
+    cache_dir_path = Path(cache_dir_path)
+    if not cache_dir_path.exists():
+        log.error(f"Cache directory {cache_dir_path} does not exist.")
+        return
+    elif not cache_dir_path.is_dir():
+        log.error(f"Cache directory {cache_dir_path} is not a directory.")
+    else:
+        log.info("Cleaning cache located at %s", cache_dir_path)
+
+    for folder in cache_dir_path.iterdir():
+        for file_path in folder.iterdir():
             try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
+                if file_path.is_file() or file_path.is_symlink():
+                    file_path.unlink()
+                elif file_path.is_dir():
                     shutil.rmtree(file_path)
             except Exception as e:
-                print(f"Failed to delete {file_path}. Exception: {e}")
-    print("Cache cleaned")
+                log.warning(f"Failed to delete {file_path}. Exception: {e}")
+    log.info("Cache cleaned successfully.")
 
 
 # Run directly from command line
