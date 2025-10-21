a3730ed6d9a603aa58821ea4c97164f36d55f9d3
Ruge Li <rugeli0605@gmail.com>
2025-10-21 11:42:28 -0700

    remove GradientData wrapper

--- cellpack/autopack/loaders/recipe_loader.py
+++ cellpack/autopack/loaders/recipe_loader.py
@@ -9,7 +9,6 @@ import cellpack.autopack as autopack
 
 from cellpack.autopack.DBRecipeHandler import DBRecipeLoader
 from cellpack.autopack.interface_objects import (
-    GradientData,
     Representations,
     default_recipe_values,
 )
@@ -238,7 +237,9 @@ class RecipeLoader(object):
         ):
             gradients = []
             for gradient_name, gradient_dict in recipe_data["gradients"].items():
-                gradients.append(GradientData(gradient_dict, gradient_name).data)
+                gradient_data = gradient_dict.copy()
+                gradient_data["name"] = gradient_name
+                gradients.append(gradient_data)
             recipe_data["gradients"] = gradients
         return recipe_data
 
