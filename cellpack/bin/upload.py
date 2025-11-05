afc5b471f7cbb821a481639fc69337f0f5ca2588
ascibisz <alli.scibisz@alleninstitute.org>
2025-11-05 14:03:25 -0800

    keep all upload functionality in upload.py, and rename client to studio

--- cellpack/bin/upload.py
+++ cellpack/bin/upload.py
@@ -1,5 +1,6 @@
 import sys
 import fire
+import json
 
 from cellpack.autopack.FirebaseHandler import FirebaseHandler
 from cellpack.autopack.DBRecipeHandler import DBUploader, DBMaintenance
@@ -32,12 +33,31 @@ def upload(
     recipe_path=None,
     config_path=None,
     db_id=DATABASE_IDS.FIREBASE,
+    studio: bool = False,
+    fields: str = None,
+    name: str = None,
 ):
     """
     Uploads a recipe to the database
 
+    :param recipe: string argument
+    path to local recipe file to upload to firebase
+    :param config: string argument
+    path to local config file to upload to firebase
+    :param db_id: string argument
+    database ID to use for the upload
+    :param studio: boolean argument
+    upload for use in cellPACK studio if True
+    :param fields: string argument
+    path to local editable fields file to upload to firebase
+    :param name: string argument
+    display name for recipe in studio selection menu
+
     :return: void
     """
+    recipe_id = ""
+    config_id = ""
+    editable_fields_ids = []
     if db_id == DATABASE_IDS.FIREBASE:
         # fetch the service key json file
         db_handler = FirebaseHandler()
@@ -47,10 +67,24 @@ def upload(
                 recipe_loader = RecipeLoader(recipe_path)
                 recipe_full_data = recipe_loader._read(resolve_inheritance=False)
                 recipe_meta_data = get_recipe_metadata(recipe_loader)
-                db_handler.upload_recipe(recipe_meta_data, recipe_full_data)
+                recipe_id = db_handler.upload_recipe(recipe_meta_data, recipe_full_data)
             if config_path:
                 config_data = ConfigLoader(config_path).config
-                db_handler.upload_config(config_data, config_path)
+                config_id = db_handler.upload_config(config_data, config_path)
+            if fields:
+                editable_fields_data = json.load(open(fields, "r"))
+                for field in editable_fields_data.get("editable_fields", []):
+                    id, _ = db_handler.upload_data("editable_fields", field)
+                    editable_fields_ids.append(id)
+            if studio:
+                recipe_metadata = {
+                    "name": name,
+                    "recipe": recipe_id,
+                    "config": config_id,
+                    "editable_fields": editable_fields_ids,
+                }
+                # Upload the combined recipe metadata to example_packings collection for studio
+                db_handler.upload_data("example_packings", recipe_metadata)
 
         else:
             db_maintainer = DBMaintenance(db_handler)
